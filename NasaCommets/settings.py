c5539d6c31e61e294fcac2598480d8ca29b38317
geekAdityaGeek <adi96saha@gmail.com>
2020-10-01 11:40:42 +0530

    Apps enrolled in Installed App section of settings.py

--- NasaCommets/settings.py
+++ NasaCommets/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Home'
+    'Home',
+    'Nearest',
+    'Fastest',
+    'Avgsize',
+    'Astroids',
 ]
 
 MIDDLEWARE = [
